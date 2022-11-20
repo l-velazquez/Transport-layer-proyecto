ae11a914f5fa24d1699f70c9c5377865305cc1c3
l-velazquez <luisfernandojavier.velazquez@upr.edu>
2022-11-20 19:04:34 -0400

    added test 2 file

---
+++ test-2.py
@@ -0,0 +1,35 @@
+from struct import *
+from socket import *
+import sys
+
+print(sys.argv[1])
+
+
+def packP(ack,checksum,message):
+    p1 = pack('B',ack)#send ack
+    p2 = pack('I',checksum)#checksum
+    p3 = pack('H',message)
+    print("sending", ack,checksum,message)
+    pf = p1 + p2 + p3
+    return pf
+
+ADDRESS = "136.145.181.51"
+PORT = 4206
+serverAddrPort = (ADDRESS,PORT)
+bufferSize = 4096
+debug = 0
+
+s = socket(AF_INET,SOCK_DGRAM)
+
+
+x = input("What is the seq num?:")
+y = input("What is the chcksum")
+z = input("What is the message len?")
+
+p = packP(x,y,z)
+
+s.sendto(p,serverAddrPort)
+
+msg = s.recv(bufferSize)
+
+print(msg.decode())
